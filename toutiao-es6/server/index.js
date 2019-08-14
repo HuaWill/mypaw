4998358dbfaf133175dfd0dc63f19829657a6bc9
whua <whua@ebay.com>
2019-08-14 09:38:02 +0800

    add nodemon for server & remove unused configurations

--- toutiao-es6/server/index.js
+++ toutiao-es6/server/index.js
@@ -1,5 +1,4 @@
 let express = require('express');
-let path = require('path');
 var cors = require('cors');
 let routes = require('./routes');
 
@@ -7,12 +6,6 @@ let app = new express();
 app.use(cors());
 
 routes.initialAppRouters(app);
-
-app.use('/dist', express.static(path.resolve(__dirname, '../dist')));
-
-app.get('/', (req, res) => {
-    res.type('text/html').sendFile(path.resolve(__dirname, '../index.html'));
-});
 
 const SERVER_PORT = '8081';
 app.set('port', SERVER_PORT);
