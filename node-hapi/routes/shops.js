d3e24899e37fc32c0f6a8197c49ec1c36a3cfa02
whua <whua@ebay.com>
2019-12-04 16:10:30 +0800

    add query parameter validation for GET shops api

--- node-hapi/routes/shops.js
+++ node-hapi/routes/shops.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+
 const GROUP_NAME = 'shops';
 
 module.exports = [
@@ -9,7 +11,15 @@ module.exports = [
     },
     config: {
       tags: ['api', GROUP_NAME],
-      description: '获取商铺列表'
+      description: '获取商铺列表',
+      validate: {
+        query: {
+          limit: Joi.number().integer().min(1).default(10)
+            .description('每页的条目数'),
+          page: Joi.number().integer().min(1).default(1)
+            .description('页码数'),
+        },
+      }
     }
   },
   {
