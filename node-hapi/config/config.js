2885efa1698e8629bdf8dd2d153dfc6c0e08985d
whua <whua@ebay.com>
2019-12-06 16:16:39 +0800

    use sequelize to create db/table/seed data ...

---
+++ node-hapi/config/config.js
@@ -0,0 +1,23 @@
+
+if (process.env.NODE_ENV === 'production') {
+  require('env2')('./.env.prod');
+} else {
+  require('env2')('./.env');
+}
+
+const { env } = process;
+
+const config = {
+  "username": env.MYSQL_USERNAME,
+  "password": env.MYSQL_PASSWORD,
+  "database": env.MYSQL_DB_NAME,
+  "host": env.MYSQL_HOST,
+  "port": env.MYSQL_PORT,
+  "dialect": "mysql",
+  "operatorsAliases": false  // 此参数为自行追加，解决高版本 sequelize 连接警告
+}
+
+module.exports = {
+  "development": config,
+  "production": config
+}
